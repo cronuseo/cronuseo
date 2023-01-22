b7d2d47c10c64bc6b8814e2af38a62e4088957af
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-01-22 23:09:09 +0530

    add some comments to main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,6 +30,7 @@ import (
 
 var Version = "1.0.0"
 
+// Default config flag.
 var flagConfig = flag.String("config", "./config/local.yml", "path to the config file")
 
 // @title          Cronuseo API
@@ -47,45 +48,53 @@ var flagConfig = flag.String("config", "./config/local.yml", "path to the config
 // @host     localhost:8080
 // @BasePath /api/v1
 func main() {
+
 	flag.Parse()
-	// load application configurations
+
+	// Load configurations for db, keto and redis.
 	cfg, err := config.Load(*flagConfig)
 	if err != nil {
 		log.Fatal("error loading config")
 		os.Exit(-1)
 	}
 
-	//connect db
+	// Connect to database.
 	db, err := sqlx.Connect("postgres", cfg.DSN)
 	if err != nil {
 		log.Fatalln("error connecting database")
 		os.Exit(-1)
 	}
 
-	//keto clients
+	// We need three keto client for checking, reading, and writing.
 
+	// Write client.
 	conn, err := grpc.Dial(cfg.KetoWrite, grpc.WithInsecure())
 	if err != nil {
 		panic("Encountered error: " + err.Error())
 	}
-
 	writeClient := rts.NewWriteServiceClient(conn)
 
+	// Read client.
 	conn, err = grpc.Dial(cfg.KetoRead, grpc.WithInsecure())
 	if err != nil {
 		panic("Encountered error: " + err.Error())
 	}
 	readClient := rts.NewReadServiceClient(conn)
 
+	// Check client.
 	conn, err = grpc.Dial(cfg.KetoRead, grpc.WithInsecure())
 	if err != nil {
 		panic("Encountered error: " + err.Error())
 	}
 	checkClient := rts.NewCheckServiceClient(conn)
 
-	clients := util.KetoClients{WriteClient: writeClient, ReadClient: readClient, CheckClient: checkClient}
+	// Create a struct to hold all keto clients.
+	clients := util.KetoClients{
+		WriteClient: writeClient,
+		ReadClient:  readClient,
+		CheckClient: checkClient}
 
-	// redis client
+	// Redis client.
 	permissionCache := cache.NewRedisCache("localhost:6379", 0, 200)
 
 	e := buildHandler(db, cfg, clients, permissionCache)
@@ -93,19 +102,27 @@ func main() {
 
 }
 
+// buildHandler builds the echo router.
 func buildHandler(db *sqlx.DB, cfg *config.Config, clients util.KetoClients, permissionCache cache.PermissionCache) *echo.Echo {
 	router := echo.New()
 	router.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowCredentials: true,
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "API_KEY"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "API_KEY"}, // API_KEY is used for permission checking SDKs
 		AllowOrigins:     []string{"http://localhost:3000"},
 	}))
+
+	// Swagger endpoint.
 	router.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// API endpoints.
 	rg := router.Group("/api/v1")
+
+	// Currently this health check endpoint is used by the run console script to check availability of the service.
 	rg.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "OK")
 	})
 
+	// Here we register all the handlers. Each handler handle jwt validation separately.
 	auth.RegisterHandlers(rg, auth.NewService(auth.NewRepository(db)))
 	check.RegisterHandlers(rg, check.NewService(check.NewRepository(clients, db), permissionCache))
 	permission.RegisterHandlers(rg, permission.NewService(permission.NewRepository(clients, db), permissionCache))
