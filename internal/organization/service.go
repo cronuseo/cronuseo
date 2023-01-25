ee1cc57773bc83e8b0b1bbdb62b1dacdc13b8a30
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-01-25 23:11:12 +0530

    add api to refresh API key

--- internal/organization/service.go
+++ internal/organization/service.go
@@ -2,6 +2,8 @@ package organization
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 
 	"github.com/shashimalcse/cronuseo/internal/entity"
 	"github.com/shashimalcse/cronuseo/internal/util"
@@ -15,6 +17,7 @@ type Service interface {
 	Query(ctx context.Context) ([]Organization, error)
 	Create(ctx context.Context, input CreateOrganizationRequest) (Organization, error)
 	Update(ctx context.Context, id string, input UpdateOrganizationRequest) (Organization, error)
+	RefreshAPIKey(ctx context.Context, id string) (Organization, error)
 	Delete(ctx context.Context, id string) (Organization, error)
 }
 
@@ -137,6 +140,32 @@ func (s service) Delete(ctx context.Context, id string) (Organization, error) {
 		return Organization{}, err
 	}
 	return organization, nil
+}
+
+// Refresh API key of the organization.
+func (s service) RefreshAPIKey(ctx context.Context, id string) (Organization, error) {
+
+	// Get organization
+	organization, err := s.Get(ctx, id)
+	if err != nil {
+		s.logger.Debug("Organization not exists.", zap.String("organization_id", id))
+		return Organization{}, &util.NotFoundError{Path: "Organization " + id + " not exists."}
+	}
+
+	// Generate new API key.
+	key := make([]byte, 32)
+
+	if _, err := rand.Read(key); err != nil {
+		return Organization{}, err
+	}
+	APIKey := base64.StdEncoding.EncodeToString(key)
+	if err := s.repo.RefreshAPIKey(ctx, APIKey, organization.ID); err != nil {
+		s.logger.Error("Error while updating organization.",
+			zap.String("organization_id", id))
+		return Organization{}, err
+	}
+	organization, err = s.Get(ctx, id)
+	return organization, err
 }
 
 // Get all organizations.
